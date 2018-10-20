36265b3b6c48909d8f7949527adf013581f6762a
AceWolf <koumaar.jayashankar@gmail.com>
2018-10-20 17:25:58 +0200

    Ball movement on ground
    Ball added
    Ground added
    Ball can move forward or backward

---
+++ Rolling Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb.AddForce(movement * speed);
+    }
+}
