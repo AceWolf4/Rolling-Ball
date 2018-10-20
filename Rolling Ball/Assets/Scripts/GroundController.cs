7ccb024a7244a57780ed86a06bdc17ac0e866422
AceWolf <koumaar.jayashankar@gmail.com>
2018-10-20 18:49:02 +0200

    Added collision to control jump

---
+++ Rolling Ball/Assets/Scripts/GroundController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour {
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("collision");
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().canJump = true;
+            other.isTrigger = false;
+        }
+    }
+
+}
--- Rolling Ball/Assets/Scripts/PlayerController.cs
+++ Rolling Ball/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,45 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
 
+    public float jumpSpeed;
+
     private Rigidbody2D rb;
+
+    private bool isJumping;
+
+    public bool canJump=false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            isJumping = true;
+            GetComponent<Collider2D>().isTrigger = true;
+        }
+        
+    }
+
+    
+
     private void FixedUpdate()
     {
+        //horizontal movement
         float moveHorizontal = Input.GetAxis("Horizontal");
-        
         Vector2 movement = new Vector2(moveHorizontal, 0);
         rb.AddForce(movement * speed);
+
+        //jump
+        if (isJumping)
+        {
+            Vector2 up = new Vector2(0, 1);
+            rb.AddForce(up*jumpSpeed);
+            isJumping = false;
+            canJump = false;
+        }
     }
 }
